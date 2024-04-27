9f28e7d36b347992245cdf15a8e847c90c1f48cb
wmalisch <malisch.will@gmail.com>
2024-04-27 14:56:17 -0400

    forgot src files

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,5 +1,5 @@
 sshpass -p "admin" scp ~/dev/ActivityTracker/main.py wmalisch@192.168.2.18:~/ActivityTracker/main.py
 sshpass -p "admin" scp ~/dev/ActivityTracker/sendOutputs.sh wmalisch@192.168.2.18:~/ActivityTracker/sendOutputs.sh
-sshpass -p "admin" scp ~/dev/ActivityTracker/src/Controller.py wmalisch@192.168.2.18:~/ActivityTracker/src/Controller.py
-sshpass -p "admin" scp ~/dev/ActivityTracker/src/Logger.py wmalisch@192.168.2.18:~/ActivityTracker/src/Logger.py
-sshpass -p "admin" scp ~/dev/ActivityTracker/src/Activity.py wmalisch@192.168.2.18:~/ActivityTracker/src/Activity.py
+sshpass -p "admin" scp ~/dev/ActivityTracker/src/controller.py wmalisch@192.168.2.18:~/ActivityTracker/src/controller.py
+sshpass -p "admin" scp ~/dev/ActivityTracker/src/logger.py wmalisch@192.168.2.18:~/ActivityTracker/src/logger.py
+sshpass -p "admin" scp ~/dev/ActivityTracker/src/activity.py wmalisch@192.168.2.18:~/ActivityTracker/src/activity.py
